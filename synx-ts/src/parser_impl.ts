b43d91293aba174c9a0d7a409ebb410210de3139
synxlang <contact@synxlang.org>
2026-05-23 13:03:20 +0800

    impl parse

--- synx-ts/src/parser_impl.ts
+++ synx-ts/src/parser_impl.ts
@@ -467,7 +467,8 @@ export class ParserImpl implements Parser {
     }
 
     parsePatternSet(node: PatternSet): ASTNode | null {
-        const start = this.input.pos;
+        let start = this.input.pos;
+        const node_start = start;
         let alt_idx = this.getPatternSetNextAltIdx(node, start);
         this.pattern_set_node_parse_stack.push({ node, pos: start, alt_idx });
 
@@ -477,24 +478,80 @@ export class ParserImpl implements Parser {
                 return null;
             }
 
-            for (let i = alt_idx; i < node.sub_nodes.length; i++) {
-                const child = this.parseSingleNode(node.sub_nodes[i]);
-                if (!this.isSuccess()) {
-                    this.input.pos = start;
-                    continue;
+            const parse_alternative = (): ASTNode | null => {
+                for (let i = alt_idx; i < node.sub_nodes.length; i++) {
+                    const child = this.parseSingleNode(node.sub_nodes[i]);
+                    if (!this.isSuccess()) {
+                        this.input.pos = start;
+                        continue;
+                    }
+                    if (node.neg_flags[i]) {
+                        this.input.pos = start;
+                        this.setError(this.input.pos, "negated alternative matched");
+                        return null;
+                    }
+                    if (child === null) {
+                        return null;
+                    }
+                    child.parser_nodes.push(node);
+                    return child;
                 }
-                if (node.neg_flags[i]) {
-                    this.input.pos = start;
-                    this.setError(this.input.pos, "negated alternative matched");
-                    return null;
-                }
-                if (child === null) {
-                    return null;
-                }
-                child.parser_nodes.push(node);
-                return child;
+                assert.ok(!this.isSuccess());
+                return null;
+            };
+
+            if (alt_idx !== 0 || node.associateby === null) {
+                return parse_alternative();
             }
-            assert.ok(!this.isSuccess());
+
+            const associateby = node.associateby;
+            const left_associate_enclosures: ASTNode[] = [];
+            do {
+                let child = parse_alternative();
+                if (this.isSuccess()) {
+                    child = child!;
+                    if (left_associate_enclosures.length > 0) {
+                        const right_associate_enclosures: ASTNode[] = [];
+                        for (let i = left_associate_enclosures.length - 1; i >= 0; i--) {
+                            const right = this.parseSingleNode(associateby[1]);
+                            if (!this.isSuccess()) {
+                                this.input.pos = node_start;
+                                return null;
+                            }
+                            right_associate_enclosures.push(right!);
+                        }
+                        const child_associate_enclosures = child.associate_enclosures;
+                        const outer_right_associate_enclosures = right_associate_enclosures.reverse();
+                        child.range = [node_start, this.input.pos];
+                        child.associate_enclosures = child_associate_enclosures === null
+                            ? [left_associate_enclosures, outer_right_associate_enclosures]
+                            : [
+                                [...left_associate_enclosures, ...child_associate_enclosures[0]],
+                                [...outer_right_associate_enclosures, ...child_associate_enclosures[1]],
+                            ];
+                        for (let i = 0; i < left_associate_enclosures.length; i++) {
+                            child.parser_nodes.push(node);
+                        }
+                    }
+                    return child;
+                }
+                if (!this.isSuccess()) {
+                    const left_start = this.input.pos;
+                    const left = this.parseSingleNode(associateby[0]);
+                    if (!this.isSuccess()) {
+                        this.input.pos = node_start;
+                        return null;
+                    }
+                    if (this.input.pos === left_start) {
+                        this.input.pos = node_start;
+                        this.setError(this.input.pos, "associateby left delimiter matched empty input");
+                        return null;
+                    }
+                    left_associate_enclosures.push(left!);
+                    start = this.input.pos;
+                }
+            } while (this.isSuccess());
+            this.input.pos = node_start;
             return null;
         } finally {
             this.pattern_set_node_parse_stack.pop();
